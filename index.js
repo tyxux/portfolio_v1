42b25e69d3b19e193d502842a29986df1f8f8f93
Mathesh <matheshwoody@gmail.com>
2022-02-09 13:50:09 +0545

    just the basic outline

---
+++ index.js
@@ -0,0 +1,10 @@
+// document.getElementById("count-el").innerText = 5    
+
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment()    {
+    count = count + 1
+    countEl.innerText = count
+    console.log(count)
+}
